96832edb9e851be01d908730aad2779a603f2956
ellawsm <92206529+ellawsm@users.noreply.github.com>
2022-04-20 16:03:59 +0800

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -14,7 +14,7 @@ $email_body = "User Name = $name.\n".
 $to = 'ellawebsitetest@gmail.com';
 
 $header = "From: $email_from \r\n";
-$headers . = "Reply-To: $visitor_email\r\n";
+$headers = "Reply-To: $visitor_email\r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
