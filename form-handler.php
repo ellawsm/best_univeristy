78c0f78cdd59c5c1c60baadfb39f1bc09dc9dcce
ellawsm <92206529+ellawsm@users.noreply.github.com>
2022-04-20 17:09:45 +0800

    updated btn

--- form-handler.php
+++ form-handler.php
@@ -13,7 +13,7 @@ $email_body = "User Name = $name.\n".
 
 $to = 'ellawebsitetest@gmail.com';
 
-$header = "From: $email_from \r\n";
+$header = "From: $email_from\r\n";
 $headers = "Reply-To: $visitor_email\r\n";
 
 mail($to, $email_subject, $email_body, $headers);
