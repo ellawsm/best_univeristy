00a25b8ab3625790ba28d48307be8ba67cbd04a7
ellawsm <92206529+ellawsm@users.noreply.github.com>
2022-04-20 16:00:53 +0800

    update php

--- form-handler.php
+++ form-handler.php
@@ -1,16 +1,14 @@
 <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $email_from = '92206529+ellawsm@users.noreply.github.com ';
 
-$email_subject = 'New Form Submission';
+$email_subject = 'New Question';
 
 $email_body = "User Name = $name.\n".
                 "User Email = $visitor_email.\n".
-                "Subject = $subject.\n".
                 "User Message = $message.\n";
 
 $to = 'ellawebsitetest@gmail.com';
@@ -18,7 +16,7 @@ $to = 'ellawebsitetest@gmail.com';
 $header = "From: $email_from \r\n";
 $headers . = "Reply-To: $visitor_email\r\n";
 
-mail($to, $email_subject, $email_body, $headers)
+mail($to, $email_subject, $email_body, $headers);
 
 header("Location: contact.html")
 
